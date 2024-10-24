ad5f8ced5ccc7d16522de0a2ac76031d0aa4932e
Hamzabaloch08 <hb.developer08@gmail.com>
2024-10-24 14:34:19 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+// assignment no: 1
+
+// const firstName = 'Hamza';
+// const last_name = 'Baloch';
+// let address = 'Main Street';
+// address = 'First Street';
+// console.log(`Name: ${firstName} ${last_name}`)
+// console.log(`Address: ${address}`)
+
+
+
+
+
+
+// assignment no: 2
+
+// let score1 = 20;
+// let score2 = 40;
+// let score3 = 60;
+// let sum = score1 + score2 + score3;
+// let totalScore = sum;
+// let averageScore = 40;
+// console.log(`Total Score: ${totalScore}`);
+// console.log(`Average Score: ${averageScore}`);
+
+
+
+
+
+
+// assignment no: 3
+
+// let plates = 20; 
+// let people = 7
+// let platesPerPerson = plates % people;
+// let remainingPlates = platesPerPerson
+// let result = `${remainingPlates} plates available`
+// console.log(result);
